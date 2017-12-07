5ebc0a496cf83e6d8c45caef2d53ed8f90ca0dbd
nnnik <nnnik@web.de>
2017-12-07 17:17:00 +0100

    TickControl verbessert
    -DEINITIALIZATION Stage hinzugefügt
    -Beenden und Starten des Spiels ist jetzt Möglich
    -Starten und Stoppen umbenannt in Play und Pause

--- Animal-Jumper/src/de/thm/iem/swtproject17/jumpnrun/core/MessageHandler.java
+++ Animal-Jumper/src/de/thm/iem/swtproject17/jumpnrun/core/MessageHandler.java
@@ -2,9 +2,10 @@ package de.thm.iem.swtproject17.jumpnrun.core;
 
 import java.util.ArrayList;
 
-/* Der MessageHandler ist eine Klasse welche Events und ähnliches ( z.B. Kollisionen und das auslösen von Items ) 
+/* Der MessageHandler ist eine Klasse welche Events und Ã¤hnliches ( z.B. Kollisionen und das auslï¿½sen von Items ) 
  * in Form von Nachrichten sammelt und diese Nachrichten dann behandelt wenn es den Befehl dazu bekommt
- * Die Klasse ist ein Singleton d.h. lässt sich die einzige instanz über MesageHandler.getInstance() erhalten
+ * Die Klasse ist ein Singleton d.h. lÃ¤sst sich die einzige instanz Ã¼ber MesageHandler.getInstance() erhalten
+ * Zudem muss die Klasse Ã¼ber MessageHandler.startUp() gestartet sein bevor eine Instanz erhalten werden kann
  */
 
 public class MessageHandler {
@@ -12,30 +13,38 @@ public class MessageHandler {
 	private static MessageHandler instance; //zum Speichern der Instanz
 	ArrayList<Message> messages = null;    //zum Speichern der Messages
 	
-	protected MessageHandler() { //Konstruktor 
-		messages = new ArrayList<Message>();
+	public static void shutdown() {
+		instance = null;
 	}
 	
-	public static MessageHandler getInstance() { //gibt die instanz zurück
+	public static void startup() {
+		instance = new MessageHandler();
+	}
+	
+	public static MessageHandler getInstance() { //gibt die instanz zurÃ¼ck
 		if ( instance == null ) {
-			instance = new MessageHandler();
+			new IllegalStateException( "Trying to get instance of MessageHandler without using startup" );
 		}
 		return instance;
 	}
 	
-	public void addCollision( Entity en1, Entity en2 ) { //wird von der Physics Klasse benutzt um Kollisionen hinzuzufügen
+	protected MessageHandler() { //Konstruktor 
+		messages = new ArrayList<Message>();
+	}
+	
+	public void addCollision( Entity en1, Entity en2 ) { //wird von der Physics Klasse benutzt um Kollisionen hinzuzufÃ¼gen
 		messages.add( new Collision( en1, en2 ) );
 	}
 	
 	public void handleMessages() { //Befehl um alle Nachrichten zu behandeln
-		for( Message message : messages ) { //Für alle Nachrichten
+		for( Message message : messages ) { //FÃ¼r alle Nachrichten
 			message.handleMessage(); //behandle Nachricht
 		}
-		messages = new ArrayList<Message>(); //Setze die ArrayList mit den Nachrichten zurück
+		messages = new ArrayList<Message>(); //Setze die ArrayList mit den Nachrichten zurÃ¼ck
 	}
 	
 	abstract class Message { //abstrakte Nachricht Klasse von der alle konkreten Nachrichten Klassen erben
-		abstract void handleMessage(); //definiert das alle Nachrichten zumindest eine Methode zum behandeln haben müssen
+		abstract void handleMessage(); //definiert das alle Nachrichten zumindest eine Methode zum behandeln haben mï¿½ssen
 	}
 	
 	private class Collision extends Message { //Collision Klasse - ist die Nachrichtenklasse welche eine Kollision darstellt
@@ -48,7 +57,7 @@ public class MessageHandler {
 			this.en2 = en2;
 		}
 		
-		public void handleMessage() { //und ruft dann später eine onCollide auf solbald sie behandelt wird
+		public void handleMessage() { //und ruft dann spÃ¤ter eine onCollide Methode auf solbald sie behandelt wird
 			en1.onCollide( en2 );
 			en2.onCollide( en1 );
 		}
--- Animal-Jumper/src/de/thm/iem/swtproject17/jumpnrun/core/TickControl.java
+++ Animal-Jumper/src/de/thm/iem/swtproject17/jumpnrun/core/TickControl.java
@@ -4,8 +4,8 @@ import de.thm.iem.swtproject17.jumpnrun.input.Input;
 import de.thm.iem.swtproject17.jumpnrun.server.Server;
 
 
-/* Die TickControl ist eine Klasse die dafür zuständig ist, dass der Game-Loop ausgeführt wird.
- * d.h. Sorgt sie Schrittweise dafür, dass nach dem einlesen der Eingabe die Physikalischen Berechnungen kommen
+/* Die TickControl ist eine Klasse die dafï¿½r zustï¿½ndig ist, dass der Game-Loop ausgefï¿½hrt wird.
+ * d.h. Sorgt sie Schrittweise dafï¿½r, dass nach dem einlesen der Eingabe die Physikalischen Berechnungen kommen
  * und danach die Ausgabe und danach wieder die Eingabe...
  */
 
@@ -13,7 +13,7 @@ public class TickControl {
 	
 	private static TickControl instance = null; //Die TickControl Klasse ist ein Singleton und die einzige Instanz wird hier gespeichert
 	
-	private boolean hasServer = false; //Speichert ob die TickControl Klasse den Server anstoßen soll oder nicht
+	private boolean hasServer = false; //Speichert ob die TickControl Klasse den Server anstoï¿½en soll oder nicht
 	private boolean serverReady = false; //Speichert ob der Server mit einer Stufe fertig ist
 	private boolean inputReady  = false; //Speichert ob die Eingabe mit einer Stufe fertig ist
 	private boolean videoReady  = false; //Speichert ob die VideoAusgabe mit einer Stufe fertig ist
@@ -23,17 +23,18 @@ public class TickControl {
 	private double frameTime,lastFrameTime; //speichert die Zeit seit dem letzten tick
 	
 	private boolean inStage = true; //Speichert ob die TickControl innerhalb einer stage ist oder nicht
+	private boolean ended = true; //speichert ob das Spiel beendet wurde oder nicht
 	private boolean stopped = true; //Speichert ob der GameLoop gestartet oder gestoppt ist
 	private Stage currentStage = null; //Speichert die momentane Stufe bereits betreten wurde oder noch nicht
 	
-	/* Die einzelnen Schritte innerhalb eines Ticks die dazu nötig sind um ein Bild korrekt zu Zeichnen 
+	/* Die einzelnen Schritte innerhalb eines Ticks die dazu nï¿½tig sind um ein Bild korrekt zu Zeichnen 
 	 * werden innerhalb der TickControl Klasse in diesem enum gesammelt.
-	 * Wenn eine Stufe begonnen wird wird code ausgeführt um die in dieser Stufe verwendeten externen Module ( wie z.B. die AudioAusgabe )
-	 * davon zu informieren, dass sie die in in dieser Stufe geplanten Aktionen ausführen müssen
+	 * Wenn eine Stufe begonnen wird wird code ausgefï¿½hrt um die in dieser Stufe verwendeten externen Module ( wie z.B. die AudioAusgabe )
+	 * davon zu informieren, dass sie die in in dieser Stufe geplanten Aktionen ausfï¿½hren mï¿½ssen
 	 * 
 	 * Sobald diese Aktionen beendet sind informieren diese Module wieder diese Klasse.
 	 * Daraufhin wird dann geschaut ob alle Module welche in dieser Stufe verwendet werden beendet sind.
-	 * Wenn dies der Fall ist wird code ausgeführt um die Stufe zu beenden, die nächte Stufe wird bestimmt und das ganze beginnt von vorne 
+	 * Wenn dies der Fall ist wird code ausgefï¿½hrt um die Stufe zu beenden, die nï¿½chte Stufe wird bestimmt und das ganze beginnt von vorne 
 	 */
 	private enum Stage { 
 		INPUT { //INPUT Stage hier erhlaten alle Module welche Informationen zum Spielkern geben das Sagen ( Eingabe, Server )
@@ -41,7 +42,7 @@ public class TickControl {
 				TickControl.instance.setFrameTime();
 				TickControl.instance.inputStageReady(); // ruft einfach nur eine Methode der Klasse TickControl auf
 			}
-			public boolean isFinished() { //wird benutzt um zu überprüfen ob diese Stufe beendet ist
+			public boolean isFinished() { //wird benutzt um zu ï¿½berprï¿½fen ob diese Stufe beendet ist
 				TickControl instance = TickControl.instance;
 				return instance.inputReady && ( !instance.hasServer || instance.serverReady ); 
 			}
@@ -72,28 +73,33 @@ public class TickControl {
 				return INPUT; //beginnt wieder mit der INPUT Stage
 			}
 		},
-		INITIALIZATION { //INITIALIZATION Stage, ist eine besondere Stage welche eingeleitet ist, wenn das eigentlich Spiel noch nicht
+		DEINITIALIZATION { //DEINITIALIZATION Stage, ist eine besondere Stage welche eingeleitet ist, wenn das eigentlich Spiel beendet ist
 			//gestartet ist und auch zum Beenden eines laufenden Spiels genutzt wird.
 			public void enter() { //beim Betreten dieser Stage werden alle externen Module heruntergefahren
 				TickControl.instance.shutdownServices();
 			}
 			public boolean isFinished() {
-				return true; //Noch nicht fertig
+				return !instance.ended;
 			}
-			@Override
-			public void leave() { //beim Verlassen der Stage werden alle externen Module hochgefahren
+			public Stage getNext() {
+				return INITIALIZATION; //geht weiter zur INITIALIZATION stage
+			}
+		},
+		INITIALIZATION { //INITIALIZATION Stage, ist eine besondere Stage welche eingeleitet wird, wenn das eigentlich Spiel noch nicht
+			//gestartet ist und gerade hochgefahren wird. In dieser Stage sollen alle Spieler erzeugt werden - die Levelgeneration richtig eingestellt werden etc.
+			public void enter() { //beim Betreten dieser Stage werden alle externen Module hochgefahren
 				TickControl.instance.startupServices();
+			}
+			public boolean isFinished() {
+				return true; //Noch nicht fertig
 			}
 			public Stage getNext() {
 				return INPUT; //beginnt dann mit der INPUT Stage
 			}
 		};
 		abstract void enter(); // wird aufgerufen sobald eine Stage betreten wird
-		abstract boolean isFinished(); // wird aufgerufen um zu überprüfen ob eine Stage beendet ist
-		abstract Stage getNext(); // wird aufgerufen um die nächste stage zu erhalten
-		public void leave() { // wird aufgerufen sobald eine Stage verlassen wird  
-			return; // da normalerweise beim Verlassen einer Stage nichts passiert habe ich dies hier beschreiben
-		}
+		abstract boolean isFinished(); // wird aufgerufen um zu ï¿½berprï¿½fen ob eine Stage beendet ist
+		abstract Stage getNext(); // wird aufgerufen um die nï¿½chste stage zu erhalten
 	};
 	
 	static public TickControl getInstance() { //TickControl ist ein Singleton: d.h. kann man auf diese Methode zugreifen
@@ -104,53 +110,90 @@ public class TickControl {
 	}
 
 	public void inputFinished() { //wird vom Eingabe Modul aufgerufen um zu signalisieren, dass es fertig ist
+		if ( inputReady )
+			new IllegalStateException( "Overlapping messages - inputReady already ready" );
 		inputReady = true; //dies wird gespeichert
-		progressStage(); //und danach wird der Mechanismus zum Einleiten der nächsten Stage eingeleitet
+		progressStage(); //und danach wird der Mechanismus zum Einleiten der nï¿½chsten Stage eingeleitet
 	}
 	
 	public void serverFinished() { //wird vom Server Modul aufgerufen um zu signalisieren, dass es fertig ist
+		if ( serverReady )
+			new IllegalStateException( "Overlapping messages - serverReady already ready" );
 		serverReady = true;
 		progressStage();
 	}
 	
 	public void videoFinished() { //wird vom Video Modul aufgerufen um zu signalisieren, dass es fertig ist
+		if ( videoReady )
+			new IllegalStateException( "Overlapping messages - videoReady already ready" );
 		videoReady = true;
 		progressStage();
 	}
 	
 	public void audioFinished() { //wird vom Audio Modul aufgerufen um zu signalisieren, dass es fertig ist
+		if ( audioReady )
+			new IllegalStateException( "Overlapping messages - audioReady already ready" );
 		audioReady = true;
 		progressStage();
 	}
 	
 	public void internalFinished() { //wird vom InternalHandler aufgerufen sobald es fertig ist
+		if ( internalReady )
+			new IllegalStateException( "Overlapping messages - internalReady already ready" );
 		internalReady = true;
 		progressStage();
 	}
 	
-	public void start() { //startet die TickControl wenn sie gestoppt ist
+	public void initializationFinished() { //wird von der GameControl aufgerufen sobald sie fertig ist mit dem Initialisieren
+		if ( currentStage != Stage.INITIALIZATION )
+			new IllegalStateException( "Overlapping messages - initializationReady already ready" );
+		stopped = false;
+		progressStage();
+		stopped = true;
+	}
+	
+	public void play() { //startet die TickControl wenn sie gestoppt ist
 		if ( !stopped )
 			new IllegalStateException( "Trying to start already started TickControl" );
 		stopped = false;
 		progressStage();
 	}
 	
-	public void stop() { //stoppt ( pausiert ) die TickControl wenn sie gestartet ist
+	public void pause() { // pausiert die TickControl wenn sie gestartet ist
 		if ( stopped )
-			new IllegalStateException( "Trying to stop an already started TickControl" );
+			new IllegalStateException( "Trying to stop an already stopped TickControl" );
 		stopped = true;
 	}
 	
-	public boolean getStopped() { //gibt zurükc ob die TickControl gestartet oder gestoppt ist
+	public void endGame() { //beendet ein Spiel falls es gestartet ist oder wird
+		if ( ended )
+			new IllegalStateException( "Trying to end an already ended game" );
+		stopped = true;
+		ended = true;
+		if ( currentStage == Stage.INITIALIZATION ) {
+			currentStage = Stage.DEINITIALIZATION;
+			if ( inStage )
+				currentStage.enter();
+			else
+				inStage = true;
+		}
+	}
+	
+	public void initializeGame() { //initialisiert ein Spiel und bewegt das Spiel in den INITIALIZATION Stage
+		ended = false;
+		progressStage();
+	}
+	
+	public boolean getStopped() { //gibt zurÃ¼ck ob die TickControl gestartet oder gestoppt ist
 		return stopped;
 	}
 	
-	public boolean hasServer() { //gibt zurück, ob die TickControl den Server verwendet oder nicht
+	public boolean hasServer() { //gibt zurÃ¼ck, ob die TickControl den Server verwendet oder nicht
 		return hasServer;
 	}
 	
 	public void setServerState( boolean newHasServer ) { //aktiviert oder deaktiviert den Server
-		if ( currentStage != Stage.INITIALIZATION ) //funktioniert nur während der INITILIZATION Stage
+		if ( currentStage != Stage.INITIALIZATION ) //funktioniert nur wÃ¤hrend der INITILIZATION Stage
 			new IllegalStateException( "Trying to activate Server outside of INITIALIZATION Stage" );
 		hasServer = newHasServer;
 	}
@@ -158,20 +201,25 @@ public class TickControl {
 	protected TickControl() {
 		stopped = true;
 		inStage = true;
-		currentStage = Stage.INITIALIZATION;
+		ended   = true;
+		currentStage = Stage.DEINITIALIZATION;
 	}
 	
-	private void progressStage() { //wird aufgerufen um die nächste Stage zu beginnen 
+	private void progressStage() { //wird aufgerufen um die nï¿½chste Stage zu beginnen 
 		if ( !inStage ) {
 			if ( !stopped ) {
 				currentStage.enter();
 				inStage = true;
 			}
-		} else if ( currentStage.isFinished() ) { //dafür wird zuerst überprüft ob die Jetzige beendet ist
-			currentStage.leave(); //wenn dies der Fall ist wird die jetzige Stage verlassen
-			currentStage = currentStage.getNext(); //die nächste Stufe wird herausgesucht
+		} else if ( currentStage.isFinished() ) { //dafï¿½r wird zuerst ï¿½berprï¿½ft ob die Jetzige beendet ist
+			currentStage = currentStage.getNext(); //die nï¿½chste Stufe wird herausgesucht
 			inStage = false;
 			if ( !stopped ) { //und wenn die TickControl nicht gestoppt ist wird diese sofort betreten
+				currentStage.enter();
+				inStage = true;
+			}
+			else if ( ended ) {
+				currentStage = Stage.DEINITIALIZATION;
 				currentStage.enter();
 				inStage = true;
 			}
@@ -240,12 +288,16 @@ public class TickControl {
 	
 	private void shutdownServices()
 	{
+		EntityControl.shutdown();
+		MessageHandler.shutdown();
 		//Not finished
 	}
 	
 	private void startupServices()
 	{
 		lastFrameTime = frameTime = currentSeconds();
+		EntityControl.startup();
+		MessageHandler.startup();
 		//Not finished
 	}
 	
